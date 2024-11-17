efcf8fae7740273e93d2e8c744f1525cf1689265
Atul Kale <kaleatul9@gmail.com>
2024-11-18 01:33:54 +0530

    Updated the name of the project.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ export default function Header({ children }) {
       <div id="main-header-loading">{fetching > 0 && <progress />}</div>
       <header id="main-header">
         <div id="header-title">
-          <h1>React Events</h1>
+          <h1>Event Verse</h1>
         </div>
         <nav>{children}</nav>
       </header>
